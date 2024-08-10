1338d00c4e2e88b07cba8984ba208f5af8ea338d
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-08-10 10:43:47 -0300

    feat: adding style

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Heart } from "lucide-react";
+
 import { products } from "./data.json";
 
 function App() {
@@ -5,22 +7,41 @@ function App() {
 
   return (
     <main>
-      <div className="App flex">
+      <div className="App flex gap-32 items-center justify-center mx-40 mt-40 w-4/5">
         {productsList &&
           productsList.map((product) => (
-            <div key={product.id}>
-              <img src={product.image} />
-              <h1>{product.title}</h1>
-              <span>{product.totalPrice}</span>
-              <span>{product.discountedPrice}</span>
-              <span>
-                em até{" "}
-                <strong>
-                  {product.installments}x de R$ {product.installmentsPrice}
-                </strong>{" "}
-                sem juros
-              </span>
-            </div>
+            <article
+              key={product.id}
+              className="bg-white rounded-lg p-8 space-y-8"
+            >
+              <div className="relative">
+                <div className="absolute right-0 h-12 w-12 flex items-center justify-center bg-grayscale-200 rounded-full transition-all cursor-pointer group hover:bg-pink-b8one-100">
+                  <Heart className="group-hover:text-red-b8one-500 group-hover:transition-all" />
+                </div>
+                <img src={product.image} className="max-w-60" />
+              </div>
+              <h1 className="font-medium text-sm text-grayscale-900 line-clamp-3">
+                {product.title}
+              </h1>
+              <div>
+                <span className="block font-medium text-sm text-grayscale-600 line-through tracking-[-0.07px]">
+                  R$ {product.totalPrice}
+                </span>
+                <span className="text-xl text-red-b8one-500 font-semibold tracking-[-.1px]">
+                  R$ {product.discountedPrice}
+                </span>
+                <span className="block text-xs font-medium text-grayscale-700 tracking-[-0.06px]">
+                  em até{" "}
+                  <strong className="font-medium text-grayscale-900">
+                    {product.installments}x de R$ {product.installmentsPrice}
+                  </strong>{" "}
+                  sem juros
+                </span>
+              </div>
+              <button className="bg-green-b8one-400 block w-full px-6 py-3 rounded-[5px] text-white text-base font-bold tracking-[-0.08px] transition-all hover:bg-green-b8one-600">
+                ADICIONAR
+              </button>
+            </article>
           ))}
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "grayscale-200": "#F2F3F6",
+        "grayscale-300": "#E9EBEE",
+        "grayscale-600": "#B5B5B6",
+        "grayscale-700": "#8F8F8F",
+        "grayscale-900": "#1C1C1C",
+        "pink-b8one-100": "#FEE5EC",
+        "red-b8one-500": "#DA4B4F",
+        "green-b8one-400": "#40B25C",
+        "green-b8one-600": "#1C6C3E",
+      }
+    },
   },
   plugins: [],
 }
