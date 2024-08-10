9cc8909dc32a795d9cdaefd360fd12def4b1bc2e
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-08-10 10:54:43 -0300

    fix: putting fix height to product name

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,23 @@ function App() {
 
   return (
     <main>
-      <div className="App flex gap-32 items-center justify-center mx-40 mt-40 w-4/5">
+      <div className="App flex gap-32 items-center justify-center mx-40 my-40 w-4/5">
         {productsList &&
           productsList.map((product) => (
             <article
               key={product.id}
-              className="bg-white rounded-lg p-8 space-y-8"
+              className="bg-white rounded-lg p-8 space-y-8 w-min"
             >
               <div className="relative">
                 <div className="absolute right-0 h-12 w-12 flex items-center justify-center bg-grayscale-200 rounded-full transition-all cursor-pointer group hover:bg-pink-b8one-100">
                   <Heart className="group-hover:text-red-b8one-500 group-hover:transition-all" />
                 </div>
-                <img src={product.image} className="max-w-60" />
+                <img
+                  src={product.image}
+                  className="max-w-60 h-60 object-contain"
+                />
               </div>
-              <h1 className="font-medium text-sm text-grayscale-900 line-clamp-3">
+              <h1 className="font-medium text-sm text-grayscale-900 h-[60px] line-clamp-3">
                 {product.title}
               </h1>
               <div>
